1750307b0c5d1e1e044a6df86b0b8be7f1dc3b39
elo062 <eloisemecozzi@gmail.com>
2017-04-04 16:02:46 +0200

    first commit exo fini

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
-  <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css
-">
+  <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
@@ -17,20 +16,16 @@
   <header class="row">
     <div class="col-sm-12">
       <h1>Entete</h1>
-      <p>Modifiez uniquement ce fichier en ajoutant des classes pour l'exercice bootstrap, pour l'exercice flexbox, ajouter les propriétés flexbox dans le fichier app.scss et n'oubliez pas de lancer la compilation avec la commande sass:
-      </p>
-      <code>sass --watch assets/scsscss:assets/css/styles.css
-      </code>
-      <p>Bootstrap : mots clefs et classes à ajouter au div existantes pour arriver au rendu final: col-XX-XX, navbar, offset</p>
+      <p>Bootstrap : mots clefs : col-XX-XX, navbar, offset</p>
       <p>Flex -> les propriétés flex: , order:, align-items, justify-content et leurs valeurs possibles</p>
-      <p>Il faut changer les styles dans le fichier assets/css/app.scss puis lancez la commande sass pour générer le fichier assets/css/styles.css pour arriver au même résultat que les captures d'écran <b>resultat.png</b>, <b>resultat2.png</b> et <b>resultat3.png</b></p>
+      <p>Il faut changer les styles dans le fichier assets/css/app.scss puis lancez la commande sass pour générer le fichier assets/css/styles.css pour arriver au même résultat que les captures d'écran résultat.png, resultat2.png et resultat3.png</p>
     </div>
   </header>
 
   <section>
     <h2>BOOTSTRAP</h2>
     <p>Essayez navbar navbar-default et navbar-inverse sur la navigation ci-dessous :</p>
-    <nav class="">
+    <nav class=".dame4">
         <ul class="nav navbar-nav">
           <li class="active"> <a href="#">Accueil</a> </li>
           <li> <a href="#">Contact</a> </li>
@@ -38,33 +33,33 @@
     </nav>
     <p>La premiere row est divisé 2 colonnes, une de 9 et une de 3, celle de 9 contient à nouveau une row de 12 qui elle est à nouveau divisé en 3(3 colonnes de 4 donc)</p>
     <div class="row">
-      <div class="">
+      <div class="col-lg-9">
         <div class="row artistes">
-          <div class="">
+          <div class="col-lg-4">
             <img src="assets/img/1.jpg" alt="Triple R" />
             <h4>Triple R – Till The Wheels Fall Off</h4></div>
-          <div class="">
+          <div class="col-lg-4">
             <img src="assets/img/2.jpg" alt="Allez savoir" />
             <h4>Allez savoir ! Ondes électromagnétiques, quels effets sur notre santé et </h4></div>
-          <div class="">
+          <div class="col-lg-4">
             <img src="assets/img/3.jpg" alt="Soul System" />
             <h4>Soul System – le Two-Step à René</h4></div>
         </div>
       </div>
-      <div class="">Largeur 3</div>
+      <div class="col-lg-3">Largeur 3</div>
     </div>
 
 
     <div class="row">
-      <div class="">Largeur 6</div>
-      <div class="">Largeur 6</div>
+      <div class="col-lg-6">Largeur 6</div>
+      <div class="col-lg-6">Largeur 6</div>
     </div>
 
 
     <p>Mettre la berge droite completement à droite grâce à la classe offset de bootstrap (sans rajouter d'élément HTML donc)</p>
     <div class="row river">
       <div class="col-xs-3 gazon">Berge gauche</div>
-      <div class="col-xs-3 gazon">Berge droite</div>
+      <div class="col-lg-offset-6 col-xs-3 gazon">Berge droite</div>
     </div>
     <h2>FIN BOOTSTRAP</h2>
   </section>
